05fb21e475ecf0c3f53e62c67f9a5d4dd62de384
Cédric Finance <cedric.finance@finandric.com>
2014-12-25 08:50:38 +0100

    Rename the docker image

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build -t teleinfo-activemq .
+docker build -t cedricfinance/teleinfo-activemq .
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -it --name teleinfo-activemq -p 61613:61613 teleinfo-activemq
+docker run --rm -it --name teleinfo-activemq -p 61613:61613 cedricfinance/teleinfo-activemq
